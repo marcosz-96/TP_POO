2bc8017ecb74f06f9291723c26fac75a73f72ced
marcosz-96 <marcosz-96@users.noreply.github.com>
2025-09-01 21:49:41 -0300

    Primera parte del proyecto (clases vacias)

---
+++ proyectFinal/src/main/java/com/mycompany/proyectfinal/modelo/interfaces/IDetalleVentaDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.proyectfinal.modelo.interfaces;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface IDetalleVentaDAO {
+    
+}
+ 
